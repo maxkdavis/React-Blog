f084ce8ff92f7594590e2a0b5f4cb0ceb4e2433d
max davis <7098417+maxkdavis@users.noreply.github.com>
2022-03-26 14:59:23 -0500

    Custom useFetch Hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,9 @@
 import React, { useState, useEffect } from "react";
+import useFetch from "../useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState();
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    //put the fetch request inside a 1 second setTimeout to simulate a one second delay to fetch data
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-                .then((res) => {
-                    if (!res.ok) {
-                        throw Error("Unable to fetch request");
-                    }
-                    return res.json();
-                })
-                .then((data) => {
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        }, 1000);
-    }, []);
+    const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
     return (
         <div className="Home">
