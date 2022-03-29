0ef2b7f5f6c313c695bca200c6a64c7f6c3b1e91
max davis <7098417+maxkdavis@users.noreply.github.com>
2022-03-28 19:42:13 -0500

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <NavBar />
-            <div className="content">
-                <Home />
+        <Router>
+            <div className="App">
+                <NavBar />
+                <div className="content">
+                    <Home />
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
